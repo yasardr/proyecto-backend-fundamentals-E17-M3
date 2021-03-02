2d21d290e208017eb2b73f0e061c71b7ef0aec6f
Yasar Altamirano <yaltamiranoa@outlook.com>
2021-03-02 10:17:29 -0800

    change of solicitud model

--- controllers/solicitudes.js
+++ controllers/solicitudes.js
@@ -14,14 +14,14 @@ function crearSolicitud(req, res) {
 
 function obtenerSolicitudes(req, res) {
   // Simulando dos solicitudes y respondiendolos
-  var solicitud1 = new Solicitud(15, 2156, 1,456589,'02/02/2021')
-  var solicitud2 = new Solicitud(14, 2156, 3,456879,'02/02/2021')
+  var solicitud1 = new Solicitud(15, [2,3,4],456589,'02/02/2021')
+  var solicitud2 = new Solicitud(14, [5,6,7], 3,456879,'02/02/2021')
   res.send([solicitud1, solicitud2])
 }
 
 function modificarSolicitud(req, res) {
   // simulando una solicitud previamente existente que el administrador modifica
-  var solicitud1 = new Solicitud(req.params.id, 2156, 2,456589,'03/02/2021')
+  var solicitud1 = new Solicitud(req.params.id, [2,3,4],456589,'03/02/2021')
   var modificaciones = req.body
   solicitud1 = { ...solicitud1, ...modificaciones }
   res.send(solicitud1)
