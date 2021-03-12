0974e0d8d70044f917b7c5b764982f2564540a6f
Yasar Altamirano <yaltamiranoa@outlook.com>
2021-03-11 21:30:45 -0800

    preparado para el deploy en heroku con dev

--- app.js
+++ app.js
@@ -14,8 +14,11 @@ app.use(bodyParser.json());
 /*********************** Mongoose Configuration *******************************/
 const mongoose = require("mongoose");
 
+var isProduction = process.env.NODE_ENV === 'production';
+
 mongoose.connect(
-    "mongodb+srv://aaronBedu:clusterBedu24@cluster0.yfntn.mongodb.net/MediManage?retryWrites=true&w=majority"
+  process.env.MONGODB_URI, // obtiene la url de conexión desde las variables de entorno
+  { useUnifiedTopology: true, useNewUrlParser: true, useCreateIndex: true }
 );
 
 mongoose.set("debug", true);
